14e87cf24f17aa0d20c43c232237ec0fda37df37
profgabrielvelini <prof.gabrielvelini@gmail.com>
2026-09-18 15:15:01 -0300

    Update vite.config.ts
    Configurar banco D1 do painel

--- painel-desafios-empresariais/vite.config.ts
+++ painel-desafios-empresariais/vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "9d4aea5d-ad2b-47cb-a5de-adadca3c1103";
 
 const { d1, r2 } = hostingConfig;
 
